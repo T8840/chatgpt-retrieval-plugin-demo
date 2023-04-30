14c7d8c504270a24935ea97258d7f87e3c89e286
T8840 <673683677@qq.com>
2023-04-30 17:42:58 +0800

    fix: chunk exceed max_length

--- chat_utils.py
+++ chat_utils.py
@@ -3,9 +3,10 @@ import openai
 import requests
 import os
 from database_utils import query_database
-import logging
+import log
+logger = log.setup_logger(__name__)
 
-
+max_chunks_length = 2900
 
 def apply_prompt_template(question: str) -> str:
     """
@@ -46,14 +47,24 @@ def ask(user_question: str, source_id : str = None) -> Dict[str, Any]:
     # Get chunks from database.
     chunks_response = query_database(user_question,source_id=source_id)
     chunks = []
+    current_length = 0
     for result in chunks_response["results"]:
         for inner_result in result["results"]:
-            chunks.append(inner_result["text"])
+            # chunks.append(inner_result["text"])
+            # 添加前判断长度
+            chunk = inner_result["text"]
+            chunk_length = len(chunk)
+            # Check if the new chunk, including a space separator, would fit within the limit
+            if current_length + chunk_length + 1 <= max_chunks_length:
+                chunks.append(chunk)
+                current_length += chunk_length + 1  # Update the current_length with the added chunk_length and space
+            else:
+                break
 
-    logging.info("User's questions: %s", user_question)
-    logging.info("Retrieved chunks: %s", chunks)
+    logger.info("User's questions: %s", user_question)
+    logger.info("Retrieved chunks: %s", chunks)
 
     response = call_chatgpt_api(user_question, chunks)
-    logging.info("Response: %s", response)
+    logger.info("Response: %s", response)
 
     return response["choices"][0]["message"]["content"]
